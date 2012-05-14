27f5b749ad522439489a9644b7fc2e154a0ee711
pospi <pospi@spadgos.com>
2012-05-14 16:18:27 +1000

    added JSchema.isRecord() and JSchema.isModel() typechecking helper methods
    update JSchema.js class file header

--- jschema.js
+++ jschema.js
@@ -8,27 +8,19 @@
  * recieve updates / notifications when that data is modified. JSON schemas
  * are passed to new data model definitions to automatically validate data.
  *
- * The event engine implements only two types of events: change and error.
- * To cut down on callback registers, monitoring unsetting of properties can
- * be achieved with the same callback as 'change', watching for the new value
- * being undefined.
- *
  * Change events are fired in order from the deepest property modified up to
  * the topmost. Returning false from any event callback will break this chain,
  * in the same manner as cancelling DOM event bubbling.
  *
  * Usage:
  *	var MyModel = JSchema.Binding.Create(
- *						jsonSchemaDefn,
- *						{idField : 'id'}
+ *					jsonSchemaDefn,
+ *					options
+ *			  	);
+ *	var record = new MyModel(
+ *					attributes,
+ *					options
  *				);
- *	var record = new MyModel({attr : value, attr2 : value2, obj1 : { key1: value1 }, ...});
- *	record.addEvent('change', function(record) { ... });					// do something when the record changes
- *	record.addEvent('change:attr2', function(record, val, attr) { ... });	// do something when the 'attr2' property of the record changes
- *	record.addEvent('change:obj1.key1', function(record, val, attr) { ... });	// do something when the 'key1' property of the 'obj1' element of the record changes
- *	record.addEvent('error', function(record, errs) { ... });				// do something when bad data is passed to the record. If no callbacks are set, an exception will be thrown.
- *	record.set({ attr2 : value3 });
- *
  *
  * For portability, no external frameworks are required other than JSV and json2.js,
  * however jQuery is builtin for core typechecking functions. You can track these dependencies
@@ -48,8 +40,6 @@
  * @depends		JSV				https://github.com/garycourt/JSV
  * @depends		json2.js		https://github.com/douglascrockford/JSON-js, required for older browsers only
  * @author		pospi <pospi@spadgos.com>
- *
- * :TODO: handle subproperty change events
  */
 var JSchema = {
 	Validator : JSV.createEnvironment('json-schema-draft-03'),
@@ -89,6 +79,30 @@ var JSchema = {
 	getSchema : function(uri)
 	{
 		return JSchema.Validator.findSchema(uri);
+	},
+
+	/**
+	 * Determine if something is an instance of a JSchema.Binding data Model
+	 * @param  {mixed}  thing variable to check
+	 * @param  {JSchema.Binding} model (optional) model object to check whether the record is an instance of
+	 * @return {Boolean}
+	 */
+	isRecord : function(thing, model)
+	{
+		if (model) {
+			return thing instanceof model && JSchema.isModel(model);
+		}
+		return thing instanceof JSchema.Binding && (!thing.prototype || !(thing.prototype instanceof JSchema.Binding));
+	},
+
+	/**
+	 * Determine if something is a JSchema.Binding data Model
+	 * @param  {mixed}  thing variable to check
+	 * @return {Boolean}
+	 */
+	isModel : function(thing)
+	{
+		return thing instanceof JSchema.Binding && thing.prototype !== undefined && thing.prototype instanceof JSchema.Binding;
 	},
 
 	/**
